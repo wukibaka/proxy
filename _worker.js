b97afef0f6da41bdb206a7b750801ac7c2544aa0
wuki <51240031+wukibaka@users.noreply.github.com>
2023-05-20 17:24:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '3.6.wuki.moe'
+      url.hostname = '2602:fed2:7307:1028:9000:9000:9000:59.nip.io'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
