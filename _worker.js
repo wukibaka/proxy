b6f463a8acec63e308491d6aef9d784ed797f2a8
wuki <51240031+wukibaka@users.noreply.github.com>
2024-01-25 13:57:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '2602:fed2:7307:1028:9000:9000:9000:59.nip.io'
+      url.hostname = 'translate.googleapis.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
